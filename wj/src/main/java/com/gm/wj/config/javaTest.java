2f2a080e8612a602c593860fce854709222f949b
zhengkangyuan <272495222@qq.com>
2022-03-21 15:54:53 +0800

    java语法测试

---
+++ wj/src/main/java/com/gm/wj/config/javaTest.java
@@ -0,0 +1,10 @@
+package com.gm.wj.config;
+
+public class javaTest {
+  /* 第一个Java程序
+   * 它将输出字符串 Hello World
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World"); // 输出 Hello World
+  }
+}
